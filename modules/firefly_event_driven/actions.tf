04cd1e3fa21e737889d866d9d60ac3174d8a1211
Gabriel Almeida <{ID}+{username}@users.noreply.github.com>
2023-07-24 22:11:52 +0400

    Add ds and cloudhsm

--- modules/firefly_event_driven/actions.tf
+++ modules/firefly_event_driven/actions.tf
@@ -552,6 +552,39 @@ locals {
         "service": "cloudfront"
     },
     {
+      "regions": [
+          "af-south-1",
+          "ap-east-1",
+          "ap-northeast-1",
+          "ap-northeast-2",
+          "ap-northeast-3",
+          "ap-south-1",
+          "ap-southeast-1",
+          "ap-southeast-2",
+          "ca-central-1",
+          "eu-central-1",
+          "eu-north-1",
+          "eu-south-1",
+          "eu-west-1",
+          "eu-west-2",
+          "eu-west-3",
+          "me-south-1",
+          "sa-east-1",
+          "us-east-1",
+          "us-east-2",
+          "us-west-1",
+          "us-west-2"
+      ],
+      "rule-name": "cloudhsm",
+      "rules": [
+          "CreateCluster",
+          "CreateHsm",
+          "DeleteCluster",
+          "DeleteHsm"
+      ],
+      "service": "cloudhsm"
+    },
+    {
         "regions": [
             "af-south-1",
             "ap-east-1",
@@ -1369,6 +1402,37 @@ locals {
             "UpdateReplicationConfigurationTemplate"
         ],
         "service": "drs"
+    },
+    {
+      "regions": [
+          "af-south-1",
+          "ap-east-1",
+          "ap-northeast-1",
+          "ap-northeast-2",
+          "ap-northeast-3",
+          "ap-south-1",
+          "ap-southeast-1",
+          "ap-southeast-2",
+          "ca-central-1",
+          "eu-central-1",
+          "eu-north-1",
+          "eu-south-1",
+          "eu-west-1",
+          "eu-west-2",
+          "eu-west-3",
+          "me-south-1",
+          "sa-east-1",
+          "us-east-1",
+          "us-east-2",
+          "us-west-1",
+          "us-west-2"
+      ],
+      "rule-name": "ds",
+      "rules": [
+          "CreateDirectory",
+          "DeleteDirectory"
+      ],
+      "service": "ds"
     },
     {
         "regions": [

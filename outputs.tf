b11264f7beebdecf6f26e049eb3b3a58e9a5d4e5
Nir Shidlansik <nir@gofirefly.io>
2024-02-22 14:07:11 +0200

    list on module not role

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "firefly_caa_role_arn" {
-  value = len(module.firefly_aws_integration.firefly_caa_role_arn) == 1 ? module.firefly_aws_integration.firefly_caa_role_arn[0]: null
+  value = len(module.firefly_aws_integration) == 1 ? module.firefly_aws_integration[0].firefly_caa_role_arn: null
 }

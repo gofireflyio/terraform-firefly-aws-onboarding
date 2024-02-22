d0bc59f0775ec28adaaf1cc991490ba2b216f105
Nir Shidlansik <nir@gofirefly.io>
2024-02-22 14:13:08 +0200

    add role name

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
-output "firefly_caa_role_arn" {
-  value = length(module.firefly_aws_integration) == 1 ? module.firefly_aws_integration[0].firefly_caa_role_arn: null
+output "firefly_integration_role_arn" {
+  value = length(module.firefly_aws_integration) == 1 ? module.firefly_aws_integration[0].firefly_integration_role_arn: null
+}
+output "firefly_integration_role_name" {
+  value = length(module.firefly_aws_integration) == 1 ? module.firefly_aws_integration[0].firefly_integration_role_name: null
 }

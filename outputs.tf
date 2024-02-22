741dce6a86b189926d00c1d0260ebbc9e1ec4ddd
Nir Shidlansik <nir@gofirefly.io>
2024-02-22 14:08:29 +0200

    length not len

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "firefly_caa_role_arn" {
-  value = len(module.firefly_aws_integration) == 1 ? module.firefly_aws_integration[0].firefly_caa_role_arn: null
+  value = length(module.firefly_aws_integration) == 1 ? module.firefly_aws_integration[0].firefly_caa_role_arn: null
 }

5e8eaaaa62f68353429022f35540b04fd6efa541
radomarina <rado@gofirefly.io>
2025-01-30 15:49:26 +0400

    feat: 2.13.0

--- modules/stackset-onboarding/data.tf
+++ modules/stackset-onboarding/data.tf
@@ -1,16 +1,21 @@
 data "aws_organizations_organization" "current" {}
 
+
 data "aws_organizations_organizational_unit_child_accounts" "accounts" {
-  parent_id = length(var.org-ou-ids) > 0 ? data.aws_organizations_organization.current.roots[0].id : var.org-ou-id
+  for_each = toset(var.org-ou-ids)
+  parent_id = length(var.org-ou-ids) > 0 ? each.value : null
 }
+
 
 data "http" "cloudformation_template" {
   url = var.cloudformation_template_url
 }
 
-locals {
-  discovered_accounts = length(var.org-ou-ids) > 0 ? [for account in data.aws_organizations_organizational_unit_child_accounts.accounts.accounts : account if account.status == "ACTIVE"] : []
-  account_ids = [for account in local.discovered_accounts : account.id]
-  applied_accounts = length(local.discovered_accounts) > 0 ? local.account_ids : []
 
+locals {
+  account_ids = flatten([
+    for ou_id in var.org-ou-ids : data.aws_organizations_organizational_unit_child_accounts.accounts[ou_id].accounts[*].id if data.aws_organizations_organizational_unit_child_accounts.accounts[ou_id].accounts[*].status == "ACTIVE"
+  ])
+  discovered_accounts = local.account_ids
+  applied_accounts = length(local.discovered_accounts) > 0 ? local.account_ids : []
 }

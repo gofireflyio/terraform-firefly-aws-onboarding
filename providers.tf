c9f5b1b2b62fe53d5762c802dc4ca7ad02aa182f
Yuval Fatal <92246164+YuvalFireFly@users.noreply.github.com>
2025-03-18 13:37:11 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,24 +1,2 @@
-// us_east_1
 provider "aws" {
-  region     = var.region
-  profile    = var.profile
-  access_key = var.access_key
-  secret_key = var.secret_key
-  dynamic "assume_role" {
-    for_each = var.aws_assume_role_arn == "" ? toset([]) : toset([0])
-    content {
-      role_arn     = var.aws_assume_role_arn
-      session_name = var.session_name
-      external_id  = var.external_id == "" ? null : var.external_id
-    }
-  }
-  dynamic "assume_role_with_web_identity" {
-    for_each = var.aws_assume_web_identity_role_arn == "" ? toset([]) : toset([0])
-    content {
-      role_arn                = var.aws_assume_web_identity_role_arn
-      web_identity_token      = var.aws_assume_web_identity_role_token == "" ? null : var.aws_assume_web_identity_role_token
-      web_identity_token_file = var.aws_assume_web_identity_role_token_file
-    }
-  }
-
 }

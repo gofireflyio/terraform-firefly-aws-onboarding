c06c9522e56a661026f907511d77b1aed3facffd
radomarina <rado@gofirefly.io>
2025-03-20 20:03:46 +0200

    release-2.1.4

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "firefly_integration_role_arn" {
-  value = length(module.firefly_aws_integration[0]) == 1 ? module.firefly_aws_integration[0].firefly_integration_role_arn : null
+  value = length(module.firefly_aws_integration[*]) == 1 ? module.firefly_aws_integration[0].firefly_integration_role_arn : null
 }
 output "firefly_integration_role_name" {
-  value = length(module.firefly_aws_integration[0]) == 1 ? module.firefly_aws_integration[0].firefly_integration_role_name : null
+  value = length(module.firefly_aws_integration[*]) == 1 ? module.firefly_aws_integration[0].firefly_integration_role_name : null
 }
 
 output "discovered_accounts" {
